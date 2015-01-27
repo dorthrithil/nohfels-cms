ef4b311d4cb7c94ff459c06158d89d87b8fe9f32
Felix Engelmann <felix@feblog.de>
2015-01-27 16:34:58 +0100

    client initial commit

---
+++ amnohfels-client/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name amnohfelsClientApp
+ * @description
+ * # amnohfelsClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('amnohfelsClientApp', []);
