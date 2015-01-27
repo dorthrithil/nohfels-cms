d28e3fcbd269607a5cbcb7e2bc88e7b5d85d4eae
Felix Engelmann <felix@feblog.de>
2015-01-27 19:36:11 +0100

    set server charset utf-8

--- amnohfels-server/index.php
+++ amnohfels-server/index.php
@@ -3,17 +3,22 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 $conn = new mysqli("localhost", "root", "", "amnohfels");
+
+/* change character set to utf8 */
+if (!$conn->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $conn->error);
+}
+
 $result = $conn->query("SELECT content, title, position FROM sections WHERE topic = 'cafe' ORDER BY position ASC");
 
 $outp = array();
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     $section = new stdClass();
     $section->title = $rs['title'];
-    $section->content = $rs['title'];
+    $section->content = $rs['content'];
     $outp[] = $section;
 }
 
 $conn->close();
-
 echo json_encode($outp);
 ?>
