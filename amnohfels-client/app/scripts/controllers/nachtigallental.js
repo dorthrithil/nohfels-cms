ba63e17b7bf1947540fa85b0afe87119690d5bfc
Felix Engelmann <felix@feblog.de>
2015-02-04 00:06:16 +0100

    responsive background image size in parallax modules

--- amnohfels-client/app/scripts/controllers/nachtigallental.js
+++ amnohfels-client/app/scripts/controllers/nachtigallental.js
@@ -17,6 +17,7 @@ angular.module('amnohfelsClientApp')
         $scope.response[0].type = 'parallax-module';
         $scope.response[0].data.title = 'XXXGrillplatz im Nachtigallental';
         $scope.response[0].data.caption = 'XXXEin ungeheim spaßiger Spaß für die ganze Familie';
+        $scope.response[0].data.bgImgSrc = 'images/parallax/sample.jpg';
         $scope.response[1].type = 'text-module';
         $scope.response[1].data.title = 'XXXUnser Grillplatz';
         $scope.response[1].data.content = 'XXXHier steht ein Text<br> über den Grillplatz<div style="height: 700px"></div>';
@@ -25,4 +26,5 @@ angular.module('amnohfelsClientApp')
         $scope.response[2].type = 'parallax-module';
         $scope.response[2].data.title = 'XXXHier ist ein Titel';
         $scope.response[2].data.caption = 'XXXDas ist ja toll <br> lalala';
+        $scope.response[2].data.bgImgSrc = 'images/parallax/sample2.jpg';
   });
--- amnohfels-client/app/scripts/directives/parallax-module.js
+++ amnohfels-client/app/scripts/directives/parallax-module.js
@@ -8,7 +8,7 @@
  */
 
 angular.module('amnohfelsClientApp')
-    .directive('parallaxModule', function () {
+    .directive('parallaxModule', function ($window, $document) {
         return {
             templateUrl: 'views/parallax-module.html',
             restrict: 'E',
@@ -18,6 +18,21 @@ angular.module('amnohfelsClientApp')
             require: '^scaffoldModules',
             link: {
                 pre: function(scope, element, attrs, scaffoldModules){
+
+                    //TODO comment this
+                    //TODO refresh on window size change
+                    scope.calcBgImgSize = function(imgHeight, imgWidth){
+                        var strechedImgHeight = imgHeight * ($document.width() / imgWidth);
+                        var nominalValue = $document.height() * element.children().attr('data-stellar-background-ratio') + imgHeight;
+                        return (strechedImgHeight > nominalValue) ? '100%' : 'auto ' + nominalValue + 'px';
+                    };
+                    var bgImg = new Image();
+                    bgImg.onload = function() {
+                        element.children().css('background-image', 'url(../' + scope.data.bgImgSrc + ')');
+                        element.children().css('background-size', scope.calcBgImgSize(this.height, this.width));
+                    };
+                    bgImg.src = scope.data.bgImgSrc;
+
                     scope.notifyLoaded = scaffoldModules.notifyElementLoaded();
                 }
             },
