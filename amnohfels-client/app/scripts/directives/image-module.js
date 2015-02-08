9e1146d556d164bc3ff4b9c75425b5ea4e10a7fb
Felix Engelmann <felix@feblog.de>
2015-02-08 08:00:57 +0100

    started lightbox

--- amnohfels-client/app/scripts/directives/image-module.js
+++ amnohfels-client/app/scripts/directives/image-module.js
@@ -7,12 +7,33 @@
  * # imageModule
  */
 angular.module('amnohfelsClientApp')
-  .directive('imageModule', function () {
+    .directive('imageModule', function ($timeout, $compile) {
         return {
             templateUrl: 'views/image-module.html',
             restrict: 'E',
             scope: {
                 data: '='
+            },
+            link: function(scope){
+                var body = angular.element(document).find('body');
+                var backdrop = angular.element('<div class="lightbox-backdrop" ng-click="closeLightbox()"></div>');
+                scope.openLightbox = function(index){
+                    var image = angular.element('<img alt="" src="' + scope.data.thumbnails[index].imageSrc + '" class="lightbox-image" />');
+                    body.append($compile(backdrop)(scope));
+                    backdrop.append(image);
+                    $timeout(function () {
+                        backdrop.addClass('lightbox-backdrop-active');
+                        image.addClass('lightbox-image-active');
+                    });
+                };
+                scope.closeLightbox = function(){
+                    backdrop.removeClass('lightbox-backdrop-active');
+                    backdrop.children().removeClass('lightbox-image-active');
+                    $timeout(function () {
+                        backdrop.children().remove();
+                        backdrop.remove();
+                    }, 2);
+                };
             }
         };
     });
