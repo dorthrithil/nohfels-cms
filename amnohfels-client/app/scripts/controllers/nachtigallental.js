82d8786bb66caac4dd45388fa6f718d711c26e46
Felix Engelmann <felix@feblog.de>
2015-02-10 23:43:24 +0100

    replaced css transitions with velocity.js animations

--- amnohfels-client/app/scripts/controllers/nachtigallental.js
+++ amnohfels-client/app/scripts/controllers/nachtigallental.js
@@ -11,18 +11,18 @@ angular.module('amnohfelsClientApp')
   .controller('NachtigallentalCtrl', function ($scope) {
         $scope.response = [];
 
-        $scope.response[0] = {};
-        $scope.response[0].data = {};
-        $scope.response[0].type = 'parallax-module';
-        $scope.response[0].data.title = 'XXXGrillplatz im Nachtigallental';
-        $scope.response[0].data.caption = 'XXXEin ungeheim spaßiger Spaß für die ganze Familie';
-        $scope.response[0].data.bgImgSrc = 'images/parallax/sample.jpg';
-
 //        $scope.response[0] = {};
 //        $scope.response[0].data = {};
-//        $scope.response[0].type = 'text-module';
-//        $scope.response[0].data.title = 'XXXUnser Grillplatz';
-//        $scope.response[0].data.content = 'XXXHier steht ein Text<br> über den Grillplatz';
+//        $scope.response[0].type = 'parallax-module';
+//        $scope.response[0].data.title = 'XXXGrillplatz im Nachtigallental';
+//        $scope.response[0].data.caption = 'XXXEin ungeheim spaßiger Spaß für die ganze Familie';
+//        $scope.response[0].data.bgImgSrc = 'images/parallax/sample.jpg';
+
+        $scope.response[0] = {};
+        $scope.response[0].data = {};
+        $scope.response[0].type = 'text-module';
+        $scope.response[0].data.title = 'XXXUnser Grillplatz';
+        $scope.response[0].data.content = 'XXXHier steht ein Text<br> über den Grillplatz';
 
         $scope.response[1] = {};
         $scope.response[1].data = {};
@@ -51,10 +51,10 @@ angular.module('amnohfelsClientApp')
         $scope.response[3].data.title = 'XXXUnser Grillplatztt';
         $scope.response[3].data.content = 'XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>XXXHier steht ein Text<br> über den Grillplatz<br>';
 
-        $scope.response[4] = {};
-        $scope.response[4].data = {};
-        $scope.response[4].type = 'parallax-module';
-        $scope.response[4].data.title = 'XXXHier ist ein Titel';
-        $scope.response[4].data.caption = 'XXXDas ist ja toll <br> lalala';
-        $scope.response[4].data.bgImgSrc = 'images/parallax/sample2.jpg';
+//        $scope.response[4] = {};
+//        $scope.response[4].data = {};
+//        $scope.response[4].type = 'parallax-module';
+//        $scope.response[4].data.title = 'XXXHier ist ein Titel';
+//        $scope.response[4].data.caption = 'XXXDas ist ja toll <br> lalala';
+//        $scope.response[4].data.bgImgSrc = 'images/parallax/sample2.jpg';
   });
--- amnohfels-client/app/scripts/directives/image-module.js
+++ amnohfels-client/app/scripts/directives/image-module.js
@@ -7,7 +7,7 @@
  * # imageModule
  */
 angular.module('amnohfelsClientApp')
-    .directive('imageModule', function ($timeout, $compile, $animate) {
+    .directive('imageModule', function ($compile) {
         return {
             templateUrl: 'views/image-module.html',
             restrict: 'E',
@@ -17,36 +17,73 @@ angular.module('amnohfelsClientApp')
             link: function(scope){
                 var body = angular.element(document).find('body');
                 var backdrop = angular.element('<div class="lb-backdrop" ng-click="lbClose()"></div>');
+
                 scope.lbOpen = function(index){
+                    //TODO can i use a template for this ugly monster?
                     var image = angular.element('<img alt="" index="' + index + '" src="' + scope.data.images[index].imageSrc + '" class="lb-image" ng-click="lbNextImage()" no-propagation lb-calc-dimensions/>');
                     body.append($compile(backdrop)(scope));
                     backdrop.append($compile(image)(scope));
-                    $animate.addClass(backdrop, 'lb-backdrop-active');
-                    $animate.addClass(image, 'lb-image-active');
+                    backdrop.velocity({
+                        opacity: 1
+                    },{
+                        duration: 300
+                    });
+                    image.velocity({
+                        opacity: 1
+                    },{
+                        duration: 300,
+                        delay: 300
+                    });
                 };
+
                 scope.lbClose = function(){
-                    $animate.removeClass(backdrop, 'lb-backdrop-active');
-                    $animate.removeClass(backdrop.children(), 'lb-image-active');
-                    $timeout(function () {
-                        backdrop.children().remove();
-                        backdrop.remove();
-                    }, 150);
+                    var image = angular.element(document.querySelector('.lb-image'));
+                    image.velocity({
+                        opacity: 0
+                    },{
+                        duration: 300,
+                        complete: function() {
+                            image.remove();
+                        }
+                    });
+                    backdrop.velocity({
+                        opacity: 0
+                    },{
+                        duration: 300,
+                        delay: 300,
+                        complete: function(){
+                            backdrop.remove();
+                        }
+                    });
                 };
+
                 scope.lbNextImage = function(){
                     var oldImage = angular.element(document.querySelector('.lb-image'));
                     var oldIndex = parseInt(oldImage.attr('index'));
                     var newIndex = (scope.data.images.length - 1 === oldIndex) ? 0 : oldIndex + 1;
+                    //TODO can i use a template for this ugly monster?
                     var newImage = angular.element('<img alt="" index="' + newIndex + '" src="' + scope.data.images[newIndex].imageSrc + '" class="lb-image lb-image-active lb-image-enter-right" ng-click="lbNextImage()" no-propagation preloadable lb-calc-dimensions/>');
                     backdrop.append($compile(newImage)(scope));
                     //watch for image getting loaded, then animate it
                     scope.$watch(function(){return newImage.attr('loaded'); }, function(newValue){
                         if(newValue === 'true'){
-                            $animate.addClass(oldImage, 'lb-image-vanish-left');
-                            $animate.removeClass(newImage, 'lb-image-enter-right');
-                            $timeout(function () {
-                                oldImage.removeClass('lb-image-leave-left');//TODO destroy it!
-                                oldImage.remove();
-                            }, 150);
+                            oldImage.velocity({
+                                opacity: 0,
+                                width: '0px',
+                                left: '0px',
+                                bottom: '200%'
+                            },{
+                                duration: 300,
+                                complete: function(){
+                                    oldImage.remove();
+                                }
+                            });
+                            newImage.velocity({
+                                left: '0'
+                            },{
+                                duration: 150,
+                                delay: 150
+                            });
                         }
                     });
                 };
