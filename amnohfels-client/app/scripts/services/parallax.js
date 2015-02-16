50854b8f55aa866ae54c4c358a371b5a2db03825
Felix Engelmann <felix@feblog.de>
2015-02-16 23:08:30 +0100

    removed scrollMagic, added own parallax script

---
+++ amnohfels-client/app/scripts/services/parallax.js
@@ -0,0 +1,112 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name amnohfelsClientApp.parallax
+ * @description
+ * # parallax
+ * Service in the amnohfelsClientApp.
+ */
+angular.module('amnohfelsClientApp')
+  .service('parallax', function parallax($window, util, $timeout) {
+    var images = [];
+    var $parallaxImagesContainer = angular.element('<div class="parallax-images" />');
+
+    //TODO route change functionality
+
+    //TODO proper image sizing
+
+    function init(){
+        $('body').prepend($parallaxImagesContainer);
+        angular.element($window).bind('resize', function() {
+            util.throttle(refresh(), 10);
+        });
+        angular.element($window).bind('scroll', function() {
+            util.throttle(refresh(), 10);
+        });
+    }
+
+    this.add = function($section, bgImgSrc){
+        var $imageContainer = angular.element('<div class="parallax-image" />');
+        $parallaxImagesContainer.append($imageContainer);
+        var $image = angular.element('<img src="'+bgImgSrc+'" />');
+        $imageContainer.append($image);
+        images.push({
+            container : $imageContainer,
+            image : $image,
+            section : $section,
+            sectionOffset : null
+        });
+        $timeout(function(){
+            images[images.length - 1].sectionOffset = $section.offset().top;
+            refresh();
+        });
+
+    };
+
+    function refresh(){
+        for(var i = 0; i < images.length; i++){
+            if(inViewport(images[i].section)) {
+                images[i].container.show();
+                var sectionPosition = window.pageYOffset - images[i].sectionOffset;
+                var imagePosition = parseInt(0.75 * sectionPosition);
+                var containerPosition = -sectionPosition;
+                images[i].container.css({'transform': 'translate3d(0, ' + containerPosition + 'px, 0)'});
+                images[i].image.css({'transform': 'translate3d(0, ' + imagePosition + 'px, 0)'});
+            } else{
+                images[i].container.hide();
+            }
+        }
+    }
+
+    function inViewport($element){
+        var bounds = $element.get(0).getBoundingClientRect();
+        return bounds.top < window.innerHeight && bounds.bottom > 0;
+    }
+
+    init();
+  });
+
+
+
+
+
+
+
+
+
+//                    var bgImgHeight = 0, bgImgWidth = 0;
+//                    var parallaxRatio = 0.2;
+//
+//                    scope.calcBgImgSizes = function(imgHeight, imgWidth){
+//                        var sizes = {};
+//                        //this is the resulting height, if we strech the image to 100% window width
+//                        var strechedImgHeight = imgHeight * ($document.width() / imgWidth);
+//                        //our image has to be at least as high as the window + the extra hight needed for parallax scolling
+//                        var nominalValue = $document.height() * parallaxRatio + imgHeight;
+//                        //if it's higher, we can use 100% width (this way we can see more), otherwise use nominalValue as height
+//                        if(strechedImgHeight > nominalValue){
+//                            sizes.bg = '100%';
+//                            sizes.margin = '-50%';
+//                        } else {
+//                            sizes.bg = 'auto ' + nominalValue + 'px';
+//                            sizes.margin = '-50%';
+//                        }
+//                        return sizes;
+//                    };
+
+//Get orininal image properties and initialize background-size
+//                    var bgImg = new Image();
+//                    bgImg.onload = function() {
+//                        bgImgHeight = this.height;
+//                        bgImgWidth = this.width;
+//                        scope.refreshBackgroundSizes();
+//                    };
+//                    bgImg.src = scope.data.bgImgSrc;
+//
+//                    scope.refreshBackgroundSizes = function(){
+//                        var sizes = scope.calcBgImgSizes(bgImgHeight, bgImgWidth);
+//                        element.children().children('.parallax-image')
+//                            .css('background-size', sizes.bg);
+//                            //.css('margin-left', sizes.margin);
+//                    };
---
+++ amnohfels-client/app/scripts/services/util.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name amnohfelsClientApp.util
+ * @description
+ * # util
+ * Service in the amnohfelsClientApp.
+ */
+angular.module('amnohfelsClientApp')
+  .service('util', function util() {
+    this.throttle =  function(fn, threshhold, throttleScope) {
+        if(!threshhold){
+            threshhold = 250;
+        }
+        var last,
+            deferTimer;
+        return function () {
+            var context = throttleScope || this;
+            var now = +new Date(),
+                args = arguments;
+            if (last && now < last + threshhold) {
+                clearTimeout(deferTimer);
+                deferTimer = setTimeout(function () {
+                    last = now;
+                    fn.apply(context, args);
+                }, threshhold);
+            } else {
+                last = now;
+                fn.apply(context, args);
+            }
+        };
+    };
+  });
