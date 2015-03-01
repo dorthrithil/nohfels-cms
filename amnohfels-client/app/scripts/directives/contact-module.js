24dd0344f488e0c7bc684e2dc0e17b3a29422405
Felix Engelmann <felix@feblog.de>
2015-03-01 23:04:09 +0100

    started contact form validation logic

--- amnohfels-client/app/scripts/directives/contact-module.js
+++ amnohfels-client/app/scripts/directives/contact-module.js
@@ -7,17 +7,58 @@
  * # contactModule
  */
 angular.module('amnohfelsClientApp')
-  .directive('contactModule', function () {
+    .directive('contactModule', function ($timeout, util) {
         return {
             templateUrl: 'views/contact-module.html',
             restrict: 'E',
             scope: {
                 data: '='
             },
-            controller: function($scope){
+            link: {
+                pre: function (scope) {
+                    scope.keydownTrigger = function ($event, errorCondition, validCondition) {
+                        var $element = angular.element($event.target);
+                        scope.deIndicate($element);
+                        util.debounce(function () {
+                            scope.indicate($element, errorCondition, validCondition);
+                        }, 1000);
+                    };
+
+                    scope.blurTrigger = function ($event, errorCondition, validCondition) {
+                        var $element = angular.element($event.target);
+                        scope.indicate($element, errorCondition, validCondition);
+                    };
+
+                    scope.indicate = function($element, errorCondition, validCondition){
+                        if (errorCondition) {
+                            scope.deIndicate($element);
+                            scope.indicateError($element);
+                        }
+                        if (validCondition) {
+                            scope.deIndicate($element);
+                            scope.indicateSuccess($element);
+                        }
+                    };
+
+                    scope.indicateError = function ($element) {
+                        $element.parent().addClass('has-error');
+                        $element.next().addClass('glyphicon-remove');
+                    };
+                    scope.indicateSuccess = function ($element) {
+                        $element.parent().addClass('has-success');
+                        $element.next().addClass('glyphicon-ok');
+                    };
+                    scope.deIndicate = function ($element) {
+                        $element.parent().removeClass('has-error has-success');
+                        $element.next().removeClass('glyphicon-remove glyphicon-ok');
+                    };
+                }
+            },
+            controller: function ($scope) {
                 $scope.email = {
+                    displayError: false,
                     text: 'tests'
                 };
             }
         };
-  });
+    });
--- amnohfels-client/app/scripts/services/util.js
+++ amnohfels-client/app/scripts/services/util.js
@@ -8,7 +8,7 @@
  * Service in the amnohfelsClientApp.
  */
 angular.module('amnohfelsClientApp')
-  .service('util', function util() {
+  .service('util', function util($timeout) {
     this.throttle =  function(fn, threshhold, throttleScope) {
         if(!threshhold){
             threshhold = 250;
@@ -30,6 +30,21 @@ angular.module('amnohfelsClientApp')
                 fn.apply(context, args);
             }
         };
+    };
+
+    //TODO comment + what happens when the scope of the function changes? will the old function be executed? (e.g. when i tab into another input and start tying there)
+    var debouncedFunctionNotExecutedYet = false,
+        debouncedFunction = null;
+
+    this.debounce = function(fn, delay){
+        if (debouncedFunctionNotExecutedYet) {
+            $timeout.cancel(debouncedFunction);
+        }
+        debouncedFunction = $timeout(function () {
+            fn.apply(this);
+            debouncedFunctionNotExecutedYet = false;
+        }, delay);
+        debouncedFunctionNotExecutedYet = true;
     };
 
     this.inViewport = function($element){
